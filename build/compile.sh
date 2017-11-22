5d9578ea8268d50366dfecf916ef6809c901bb1d
Simon Wendsche <simon.wendsche@gmx.de>
2017-11-22 01:40:34 +0100

    changed gitignore, added compile script

---
+++ build/compile.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+# Delete old binary
+rm Arduino_DCT
+
+# Compile
+echo "Running CMake..."
+
+case "$1" in
+    debug)
+        echo "Creating DEBUG build"
+        cmake -DCMAKE_BUILD_TYPE=Debug ..
+        ;;
+    release)
+        echo "Creating RELEASE build"
+        cmake -DCMAKE_BUILD_TYPE=Release ..
+        ;;
+    analyze)
+        echo "Creating DEBUG build for static analysis"
+        cmake -DCMAKE_BUILD_TYPE=Debug ..
+        ;;
+    *)
+        echo "Choose debug or release build mode using one of the following commands:"
+        echo "$0 debug"
+        echo "$0 release"
+        echo "$0 analyze"
+        exit 1
+esac
+
+if [ "$1" == "analyze" ]
+then
+    scan-build make
+else
+    make -j 4
+fi
+
+# Clean up
+echo "Cleaning up..."
+rm -r CMakeFiles
+rm CMakeCache.txt cmake_install.cmake Makefile
+
+# Testrun
+./Arduino_DCT
