92b4b3dff8b9a15bec1c0f8d4aadc1967219bddf
Simon Wendsche <simon.wendsche@gmx.de>
2017-11-27 11:39:49 +0100

    add support for opengl2

--- ui/main_gl2.cpp
+++ ui/main_gl2.cpp
@@ -1,13 +1,11 @@
-// ImGui - standalone example application for Glfw + OpenGL 3, using programmable pipeline
+// ImGui - standalone example application for Glfw + OpenGL 2, using fixed pipeline
 // If you are new to ImGui, see examples/README.txt and documentation at the top of imgui.cpp.
+// We use this because stoertebeker has an ancient computer found in a dumpster
 
 #include <imgui.h>
-#include "imgui_impl_glfw_gl3.h"
+#include "imgui_impl_glfw_gl2.h"
 #include <stdio.h>
-#include <GL/gl3w.h>
 #include <GLFW/glfw3.h>
-
-#include <vector>
 
 #include "mainwindow.h"
 #include "utils.h"
@@ -17,31 +15,18 @@ static void error_callback(int error, const char* description)
     fprintf(stderr, "Error %d: %s\n", error, description);
 }
 
-GLFWwindow* setupGlfw() {
-    glfwSetErrorCallback(error_callback);
-    if (!glfwInit())
-        return nullptr;
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
-    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-    GLFWwindow* window = glfwCreateWindow(1280, 720, "DCT UI", NULL, NULL);
-    glfwMakeContextCurrent(window);
-    glfwSwapInterval(1); // Enable vsync
-    gl3wInit();
-    
-    return window;
-}
-
 int main(int, char**)
 {
     // Setup window
-    GLFWwindow* window = setupGlfw();
-    if(!window) {
-        fprintf(stderr, "Could not create GLFWwindow.\n");
+    glfwSetErrorCallback(error_callback);
+    if (!glfwInit())
         return 1;
-    }
+    GLFWwindow* window = glfwCreateWindow(1280, 720, "ImGui OpenGL2 example", NULL, NULL);
+    glfwMakeContextCurrent(window);
+    glfwSwapInterval(1); // Enable vsync
+
     // Setup ImGui binding
-    ImGui_ImplGlfwGL3_Init(window, true);
+    ImGui_ImplGlfwGL2_Init(window, true);
     
     utils::darkTheme();
     MainWindow mainWindow;
@@ -50,22 +35,22 @@ int main(int, char**)
     while (!glfwWindowShouldClose(window))
     {
         glfwPollEvents();
-        ImGui_ImplGlfwGL3_NewFrame();
-        
+        ImGui_ImplGlfwGL2_NewFrame();
+
         mainWindow.draw();
 
         // Rendering
         int display_w, display_h;
         glfwGetFramebufferSize(window, &display_w, &display_h);
         glViewport(0, 0, display_w, display_h);
-        glClearColor(0.45f, 0.55f, 0.60f, 1.00f); // Background color
+        glClearColor(0.4, 0.4, 0.4, 1.0);
         glClear(GL_COLOR_BUFFER_BIT);
         ImGui::Render();
         glfwSwapBuffers(window);
     }
 
     // Cleanup
-    ImGui_ImplGlfwGL3_Shutdown();
+    ImGui_ImplGlfwGL2_Shutdown();
     glfwTerminate();
 
     return 0;
---
+++ ui/main_gl3.cpp
@@ -0,0 +1,70 @@
+// ImGui - standalone example application for Glfw + OpenGL 3, using programmable pipeline
+// If you are new to ImGui, see examples/README.txt and documentation at the top of imgui.cpp.
+
+#include <imgui.h>
+#include "imgui_impl_glfw_gl3.h"
+#include <stdio.h>
+#include <GL/gl3w.h>
+#include <GLFW/glfw3.h>
+
+#include "mainwindow.h"
+#include "utils.h"
+
+static void error_callback(int error, const char* description)
+{
+    fprintf(stderr, "Error %d: %s\n", error, description);
+}
+
+GLFWwindow* setupGlfw() {
+    glfwSetErrorCallback(error_callback);
+    if (!glfwInit())
+        return nullptr;
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    GLFWwindow* window = glfwCreateWindow(1280, 720, "DCT UI", NULL, NULL);
+    glfwMakeContextCurrent(window);
+    glfwSwapInterval(1); // Enable vsync
+    gl3wInit();
+    
+    return window;
+}
+
+int main(int, char**)
+{
+    // Setup window
+    GLFWwindow* window = setupGlfw();
+    if(!window) {
+        fprintf(stderr, "Could not create GLFWwindow.\n");
+        return 1;
+    }
+    // Setup ImGui binding
+    ImGui_ImplGlfwGL3_Init(window, true);
+    
+    utils::darkTheme();
+    MainWindow mainWindow;
+
+    // Main loop
+    while (!glfwWindowShouldClose(window))
+    {
+        glfwPollEvents();
+        ImGui_ImplGlfwGL3_NewFrame();
+        
+        mainWindow.draw();
+
+        // Rendering
+        int display_w, display_h;
+        glfwGetFramebufferSize(window, &display_w, &display_h);
+        glViewport(0, 0, display_w, display_h);
+        glClearColor(0.45f, 0.55f, 0.60f, 1.00f); // Background color
+        glClear(GL_COLOR_BUFFER_BIT);
+        ImGui::Render();
+        glfwSwapBuffers(window);
+    }
+
+    // Cleanup
+    ImGui_ImplGlfwGL3_Shutdown();
+    glfwTerminate();
+
+    return 0;
+}
